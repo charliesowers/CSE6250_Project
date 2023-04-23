fa07db213bc4df6d86b0f059133622e70d3dadea
Jason <jason.zeng@dsm.com>
2023-04-23 14:09:39 -0400

    added custom dataset class

--- utils.py
+++ utils.py
@@ -1,7 +1,13 @@
+"""
+Most of the codes here are from HW 5
+"""
+
 import time
-import numpy as np
-import torch
 from sklearn.metrics import precision_recall_fscore_support, roc_auc_score, accuracy_score, precision_recall_curve
+import numpy as np
+from scipy import sparse
+import torch
+from torch.utils.data import TensorDataset, Dataset
 
 class AverageMeter(object):
 	"""Computes and stores the average and current value"""
@@ -92,6 +98,7 @@ def evaluate(model, device, data_loader, criterion, print_freq=10):
 	batch_time = AverageMeter()
 	losses = AverageMeter()
 	accuracy = AverageMeter()
+	auc = AverageMeter()
 
 	results = []
 
@@ -116,7 +123,7 @@ def evaluate(model, device, data_loader, criterion, print_freq=10):
 
 			losses.update(loss.item(), target.size(0))
 			accuracy.update(compute_batch_accuracy(output, target).item(), target.size(0))
-
+			auc.update(compute_batch_rocAuc(output, target).item(), target.size(0))
 			y_true = target.detach().to('cpu').numpy().tolist()
 			y_pred = output.detach().to('cpu').max(1)[1].numpy().tolist()
 			results.extend(list(zip(y_true, y_pred)))
@@ -128,4 +135,74 @@ def evaluate(model, device, data_loader, criterion, print_freq=10):
 					  'Accuracy {acc.val:.3f} ({acc.avg:.3f})'.format(
 					i, len(data_loader), batch_time=batch_time, loss=losses, acc=accuracy))
 
-	return losses.avg, accuracy.avg, results
+	return losses.avg, accuracy.avg, auc.avg, results
+
+# from homework 5
+class VisitSequenceWithLabelDataset(Dataset):
+    def __init__(self, seqs, labels, num_features):
+        """
+        Args:
+            seqs (list): list of patients (list) of visits (list) of codes (int) that contains visit sequences
+            labels (list): list of labels (int)
+            num_features (int): number of total features available
+        """
+
+        if len(seqs) != len(labels):
+            raise ValueError("Seqs and Labels have different lengths")
+
+        self.labels = labels
+        self.seqs = []
+        for seq in seqs:
+            num_visits = len(seq)
+            row = []
+            col = []
+            for i in range(num_visits):
+                row.extend([i]*len(seq[i]))
+                col.extend(seq[i])
+            data = [1] * len(col)
+            sparse_matrix = sparse.coo_matrix((data, (row, col)), shape=(num_visits, num_features))
+            self.seqs.append(sparse_matrix)
+    def __len__(self):
+        return len(self.labels)
+
+    def __getitem__(self, index):
+        # returns will be wrapped as List of Tensor(s) by DataLoader
+        return self.seqs[index], self.labels[index]
+
+
+def visit_collate_fn(batch):
+    """
+    DataLoaderIter call - self.collate_fn([self.dataset[i] for i in indices])
+    Thus, 'batch' is a list [(seq_1, label_1), (seq_2, label_2), ... , (seq_N, label_N)]
+    where N is minibatch size, seq_i is a Numpy (or Scipy Sparse) array, and label is an int value
+
+    :returns
+        seqs (FloatTensor) - 3D of batch_size X max_length X num_features
+        lengths (LongTensor) - 1D of batch_size
+        labels (LongTensor) - 1D of batch_size
+    """
+
+    # TODO: Return the following two things
+    # TODO: 1. a tuple of (Tensor contains the sequence data , Tensor contains the length of each sequence),
+    # TODO: 2. Tensor contains the label of each sequence
+    tuple_list = [(i, t.shape[0]) for i, (t, _) in enumerate(batch)]
+    sorted_tuple_list = sorted(tuple_list, key=lambda tup: tup[1], reverse=True)
+
+    max_row = sorted_tuple_list[0][1]
+    max_col = batch[0][0].shape[1]
+
+    seqs, lens, labels = [], [], []
+    for i, y in sorted_tuple_list:
+        lens.append(y)
+        labels.append(batch[i][1])
+        old_matrix = batch[i][0].toarray()
+        row_num = max_row - y
+        new_matrix = np.zeros((row_num, max_col))
+        final_matrix = np.concatenate([old_matrix, new_matrix])
+        seqs.append(final_matrix)
+
+    seqs_tensor = torch.FloatTensor(seqs)
+    lengths_tensor = torch.LongTensor(lens)
+    labels_tensor = torch.LongTensor(labels)
+
+    return (seqs_tensor, lengths_tensor), labels_tensor
